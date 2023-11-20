cbb10c3493d03b04a8eeb5ffcbeb4ee5b8e52267
Max Schoening <max@max.wtf>
2023-11-19 20:59:55 -0800

    Set up basic token auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import { Elysia } from "elysia";
+import { bearer } from "@elysiajs/bearer";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(bearer())
+  .post("/", () => "Hello World!", {
+    beforeHandle({ bearer, set }) {
+      if (!bearer) {
+        set.status = 400;
+        return "INVALID_REQUEST";
+      }
+
+      if (bearer !== process.env.API_TOKEN) {
+        set.status = 403;
+        return "UNAUTHORIZED";
+      }
+    },
+  })
+  .listen(process.env.PORT || 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
