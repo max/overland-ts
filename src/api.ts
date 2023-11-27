adcd780d6a0039533df06afb6a8b3cf38b2fe88b
Max Schoening <max@max.wtf>
2023-11-27 08:25:44 -1000

    Add location and motion data to the database

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,10 @@
 import bearer from "@elysiajs/bearer";
 import { Elysia } from "elysia";
+import { db } from "./db";
+import { locations, motions } from "./db/schema";
 import config from "./lib/config";
 import { UnauthorizedError } from "./lib/error";
-import { OverlandSchema } from "./lib/overland";
+import { OverlandSchema, OverlandSchemaType } from "./lib/overland";
 
 export const apiRouter = new Elysia().group("/api", (app) =>
   app
@@ -12,7 +14,39 @@ export const apiRouter = new Elysia().group("/api", (app) =>
         throw new UnauthorizedError("Invalid token");
       }
     })
-    .post("/locations", async ({ body }) => body, {
-      body: OverlandSchema,
-    })
+    .post(
+      "/locations",
+      async ({ body }: { body: OverlandSchemaType }) => {
+        await db.transaction(async (tx) => {
+          for (const location of body.locations) {
+            const [{ newLocationID }] = await tx
+              .insert(locations)
+              .values({
+                altitude: location.properties.altitude,
+                batteryState: location.properties.battery_state,
+                createdAt: location.properties.timestamp,
+                deviceID: location.properties.device_id,
+                horizontalAccuracy: location.properties.horizontal_accuracy,
+                latitude: location.geometry.coordinates[1],
+                longitude: location.geometry.coordinates[0],
+                speed: location.properties.speed,
+                uniqueID: location.properties.unique_id,
+                verticalAccuracy: location.properties.vertical_accuracy,
+                wifi: location.properties.wifi,
+              })
+              .returning({ newLocationID: locations.id });
+
+            for (const motion of location.properties.motion) {
+              await tx.insert(motions).values({
+                locationID: newLocationID,
+                type: motion,
+              });
+            }
+          }
+        });
+      },
+      {
+        body: OverlandSchema,
+      }
+    )
 );
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,52 @@
-import { integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const locations = sqliteTable("locations", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  createdAt: text("created_at"),
+  latitude: real("latitude"),
+  longitude: real("longitude"),
+  altitude: real("altitude"),
+  batteryState: text("battery_state"), // TODO property
+  deviceID: text("device_id"), // TODO property
+  horizontalAccuracy: real("horizontal_accuracy"),
+  speed: real("speed"),
+  uniqueID: text("unique_id"), // TODO property
+  verticalAccuracy: real("vertical_accuracy"),
+  wifi: text("wifi"), // TODO: property
 });
+
+export const locationsRelations = relations(locations, ({ many }) => ({
+  motions: many(motions),
+  properties: many(properties),
+}));
+
+export const motions = sqliteTable("motions", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  locationID: integer("location_id"),
+  type: text("type"),
+});
+
+export const motionsRelations = relations(motions, ({ one }) => ({
+  location: one(locations, {
+    fields: [motions.locationID],
+    references: [locations.id],
+  }),
+}));
+
+export const properties = sqliteTable("properties", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  locationID: integer("location_id"),
+  key: text("key"),
+  value: text("value"),
+});
+
+export const propertiesRelations = relations(properties, ({ one }) => ({
+  location: one(locations, {
+    fields: [properties.locationID],
+    references: [locations.id],
+  }),
+}));
 
 export type Location = typeof locations.$inferSelect;
 export type InsertLocation = typeof locations.$inferInsert;
